41f2be4bae04d695f6477c5786e0a06eebed8e40
wangcan <961726183@qq.com>
2019-05-28 23:27:48 +0800

    空白

---
+++ TodayNews/TodayNews/Classes/Main/Tools/NetworkTool.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkTool.swift
+//  TodayNews
+//
+//  Created by Winter on 2019/5/28.
+//  Copyright © 2019 Shirley. All rights reserved.
+//
+
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+protocol NetworkToolProtocol {
+    // MARK: *************** 首页 home **************
+    
+    // MARK: 首页顶部新闻标题的数据
+    
+    // MARK: *************** 视频 video **************
+    // MARK: *************** 我的 mine **************
+    // MARK: 我的界面 cell 的数据
+    // MARK: 我的关注数据
+    // MARK: 获取用户详情数据
+    // MARK: 已关注用户，取消关注
+    // MARK: 点击关注按钮，关注用户
+    // MARK: 点击了关注按钮，就回出现相关推荐数据
+    // MARK: 获取用户详情的动态列表数据
+    // MARK: 获取用户详情的文章列表数据
+    // MARK: 获取用户详情的问答列表数据
+    // MARK: 获取用户详情的动态详细内容，**暂未使用本方法**
+    // MARK: 获取用户详情的动态转发或引用内容，**暂未使用本方法**
+    // MARK: 获取用户详情一般的详情的评论数据
+    // MARK: 获取用户详情其他类型的详情的评论数据
+    // MARK: 获取动态详情的用户点赞列表数据
+    // MARK: 获取问答的列表数据（提出了问题）
+    // MARK: 获取问答的列表数据（提出了问题），加载更多
+ 
+    
+    // MARK: *************** 小视频    **************
+    // MARK: *************** 新年活动 **************
+}
+
+extension NetworkToolProtocol{
+    // MARK: *************** 首页 home **************
+    // MARK: *************** 视频 video **************
+    // MARK: *************** 我的 mine **************
+    // MARK: *************** 小视频    **************
+    // MARK: *************** 新年活动 **************
+}
+
+struct NetworkTool:NetworkToolProtocol {
+    
+}
